afaa6d85c019ce572128957478a5caec981e96b1
immrakpk <35355294+immrakpk@users.noreply.github.com>
2022-01-10 17:23:41 -0500

    Create fastapi_start_end_event.py

---
+++ fastapi_start_end_event.py
@@ -0,0 +1,9 @@
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
+    app.mongodb = app.mongodb_client[settings.DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
